c9281eb489cd6073e1c448d5478c88d528c19bd3
Roiboon Chaiyachit <roiboon.cha@tourkrub.co>
2020-04-04 21:54:48 +0700

    Add Home page.

---
+++ web/src/components/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <h1>CV</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et ratione ipsum maiores, alias minus praesentium modi maxime exercitationem doloribus tempora aliquid consectetur, sequi fugit necessitatibus nobis enim laudantium cupiditate dolorem!</p>
+  </main>
+</template>
+
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
