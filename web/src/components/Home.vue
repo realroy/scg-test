efc6768878129f72d075dba4a18cecae31ed22ec
Roiboon Chaiyachit <roiboon.cha@tourkrub.co>
2020-04-04 22:37:44 +0700

    Add information to Home.

--- web/src/components/Home.vue
+++ web/src/components/Home.vue
@@ -1,13 +1,169 @@
 <template>
-  <main>
-    <h1>CV</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et ratione ipsum maiores, alias minus praesentium modi maxime exercitationem doloribus tempora aliquid consectetur, sequi fugit necessitatibus nobis enim laudantium cupiditate dolorem!</p>
+  <main class="container">
+    
+    <section class="jumbotron mt-5 bg-white border rounded">
+      <h1 class="display-4">Roiboon Chaiyachit (Roy)</h1>
+      <p class="mt-4">
+        <strong>Email</strong>
+      </p>
+      <p>
+        <a href="mailto:roiboon.cc@gmail.com">roiboon.cc@gmail.com</a>
+      </p>
+      <p>
+        <strong>Location</strong>
+      </p>
+      <p>Bangkok Thailand</p>
+      <hr class="my-4" />
+      <p>
+        <strong>Education:</strong>
+      </p>
+      <p>
+        2016 - 2019
+        BE: Software and Knowledge Engineering
+        Kasetsart University
+      </p>
+    </section>
+    
+    <section class="border rounded p-5">
+      <h2>TECHNICAL SKILLS</h2>
+      <hr class="my-4" />
+      
+      <p>
+        <strong>Language: </strong>
+        TypeScript, Ruby, Python, Java, Go, HTML, CSS, SCSS
+      </p>
+      
+      <p>
+        <strong>Technology: </strong>
+        Ruby on Rails, Node.js, React.js, Next.js, Redux, React Native, Django, Vue.js, Nuxt.js, Angular, Webpack, GraphQL, PostCSS, Nginx, PM2, d3.js, Jupyter Notebook and jQuery</p>
+      <p>
+        <strong>DevOp: </strong>
+        Docker, CircleCI
+      </p>
+      
+      <p>
+        <strong>Database: </strong>
+        MySQL, Postgresql, Redis, Apache CouchDB, and Firebase Realtime DB, ElasticSearch</p>
+      <p>
+
+      <p>
+        <strong>Platforms: </strong>
+        Heroku, Google Cloud Platform, Digital Ocean and Amazon Web Service
+      </p>
+
+    </section>
+
+    <section class="border rounded p-5 mt-5">
+      <h2>WORK EXPERIENCE</h2>
+      <hr class="my-4" />
+      <article class="mt-5">
+        <div class="row">
+          <div class="col-12 col-md-4">
+            <h3>June 2019 - Now</h3>
+          </div>
+          <div class="col-12 col-md-4 text-center">
+            <h3>Full Stack Software developer</h3>
+          </div>
+          <div class="col-12 col-md-4 text-right">
+            <h3>Tourkrub</h3>
+          </div>
+        </div>
+        <p class="mt-4">
+          <ul>
+            <li>
+              Migrate client side from Ruby on Rails full stack website to Nuxt.js
+            </li>
+            <li>
+              Design and Implement api for client-side and mobile application with GraphQL
+            </li>
+            <li>
+              Integrate exist authentication api and facebook authentication in client side
+            </li>
+            <li>
+              Setup microservice projects that run on Heroku
+            </li>
+            <li>
+              Setup CI/CD pipeline for microservices
+            </li>
+            <li>
+              Contributed in online payment service
+            </li>
+            <li>
+              Optimized performance SQL queries
+            </li>
+            <li>
+              Contributed in email delivery service, mobile app notification and In app message
+            </li>
+            <li>
+              Contributed in Affiliate marketing service
+            </li>
+            <li>
+              Contributed in Referral marketing
+            </li>
+            <li>
+              All project have unit test with coverage 100%
+            </li>
+          </ul>
+        </p>
+      </article>
+      <article class="mt-5">
+        <div class="row mt-4">
+          <div class="col-12 col-md-4">
+            <h3>June 2018 - Dec 2019</h3>
+          </div>
+          <div class="col-12 col-md-4">
+            <h3 class="text-center">Software developer (Freelance)</h3>
+          </div>
+          <div class="col-12 col-md-4">
+            <h3 class="text-right">KanoonTH Studio</h3>
+          </div>
+        </div>
+        <p class="mt-4">
+          <ul>
+            <li>
+              Nakornthai Bike: Bicycle ecommerce website.
+            </li>
+            <li>
+              <a target="blank" href="https://bakeryhubth.com">bakeryhubth.com</a> Bakery delivery website.
+            </li>
+          </ul>
+        </p>
+      </article>
+
+      <article class="mt-5">
+        <div class="row">
+          <div class="col-12 col-md-4">
+            <h3>June - Dec 2018</h3>
+          </div>
+          <div class="col-12 col-md-4 text-center">
+            <h3>Software developer (Internship)</h3>
+          </div>
+          <div class="col-12 col-md-4 text-right">
+            <h3>Refinitiv (Thomson Reuters)</h3>
+          </div>
+        </div>
+        <p class="mt-4">
+          <ul>
+            <li>
+              Developed Thai Public Limited Company and person name auto tagging prototype web application from scratch.
+            </li>
+            <li>
+              Contributed in anomaly detection in data center application by introducing containerization with Docker, wizard application configuration page, and Auto recovering features.
+            </li>
+            <li>
+              Contributed in Merger and Acquisition platform to try to help companies that are seeking for interesting companies for merger or acquisition by using useful information from Refinitv API services.
+            </li>
+          </ul>
+        </p>
+      </article>
+
+    </section>
   </main>
 </template>
 
 
 <script>
 export default {
-  name: 'Home',
-}
+  name: "Home"
+};
 </script>
