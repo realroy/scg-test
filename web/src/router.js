79a76394dd2c48ea240be996abd947a6cbeb2438
Roiboon Chaiyachit <roiboon.cha@tourkrub.co>
2020-04-04 23:33:47 +0700

    Fix routes.

--- web/src/router.js
+++ web/src/router.js
@@ -8,10 +8,10 @@ export default new VueRouter({
   linkExactActiveClass: 'active',
   routes: [
     { path: "/", component: () => import('./components/Home.vue') },
-    { path: "/do-scg", component: () => import('./components/DoSCG.vue') },
+    { path: "/DOSCG", component: () => import('./components/DoSCG/DoSCG.vue') },
     { path: "/xyz", component: () => import('./components/XYZ.vue') },
     { path: "/bc", component: () => import('./components/BC.vue') },
     { path: "/google-api", component: () => import('./components/GoogleAPI.vue') },
-    { path: "/line-message-api", component: () => import('./components/DoSCG.vue') },
+    { path: "/line-message-api", component: () => import('./components/LineMessageAPI.vue') },
   ],
 });
