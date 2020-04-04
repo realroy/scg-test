e6fa5c34cbba926ab437f7b125a69bbffb8f6efe
Roiboon Chaiyachit <roiboon.cha@tourkrub.co>
2020-04-04 23:04:43 +0700

    Add Google API.

---
+++ web/src/components/GoogleAPI.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="mt-5">
+    <h1>Google API</h1>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed recusandae vitae dolores. Sapiente vitae accusamus, eligendi nemo in nostrum dolorem harum impedit voluptatem aut non dolore officiis ullam consequatur alias!
+  </div>
+</template>
