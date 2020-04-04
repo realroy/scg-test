1f3956a066f5023a91f383992a17c7088f628e46
Roiboon Chaiyachit <roiboon.cha@tourkrub.co>
2020-04-04 21:54:56 +0700

    Add Navbar.

---
+++ web/src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <router-link class="navbar-brand" to="/">SCG Test</router-link>
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <router-link class="nav-link" to="/">Home</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/doscg">DOSCG</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/finding-x-y-and-z">Finding X, Y and Z</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/finding-b-and-c">Finding B and C</router-link>
+      </li>
+      <div class="nav-item">
+        <router-link class="nav-link" to="/google-api">Google API</router-link>
+      </div>
+      <div class="nav-item">
+        <router-link class="nav-link" to="/line-message-api">Line Message API</router-link>
+      </div>
+    </ul>
+  </nav>
+</template>
