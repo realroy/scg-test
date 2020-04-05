77dc9069f1f418990d27e884561a72fb4a4bad8f
Roiboon Chaiyachit <roiboon.cha@tourkrub.co>
2020-04-05 12:53:37 +0700

    Add Embeding map.

--- web/src/components/GoogleAPI.vue
+++ web/src/components/GoogleAPI.vue
@@ -5,55 +5,26 @@
     </section>
 
     <section ref="mapContainer" class="border rounded p-5 mb-5">
-      <div id="map"></div>
     </section>
   </div>
 </template>
 
 <script>
-const scgBangsue = { lat: 13.8050023, lng: 100.5327976 };
-const centralWorld = { lat: 13.7466356, lng: 100.5371464 };
+const API_ENDPOINT = 'https://www.google.com/maps/embed/v1/directions'
+const OPTION = new URLSearchParams({
+  key: "AIzaSyBYtbzzry3PY5FviIpmwRxfw9XljsW6EfY",
+  origin: "SCG+สำนักงานใหญ่+บางซื่อ",
+  destination: "centralwOrld",
+}).toString()
 
 export default {
   mounted() {
-    const origin = JSON.stringify(scgBangsue)
-    const destination = JSON.stringify(centralWorld)
-    const apiKey = "AIzaSyBYtbzzry3PY5FviIpmwRxfw9XljsW6EfY"
-    const callbackName = "initMap"
-    const selectorId = "map"
+    const iframe = document.createElement('iframe')
+    iframe.src = `${API_ENDPOINT}?${OPTION}`
+    iframe.classList.add('border', 'w-100')
+    iframe.height = "600px"
 
-    const googleMapScript = document.createElement("script");
-    googleMapScript.async = true;
-    googleMapScript.defer = true;
-    googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=${callbackName}`;
-
-    const initMapScript = document.createElement("script");
-    initMapScript.textContent = `
-    function ${callbackName}() {
-      const directionsRenderer = new google.maps.DirectionsRenderer();
-      const directionsService = new google.maps.DirectionsService();
-
-      const $map = document.getElementById("${selectorId}");
-
-      const map = new google.maps.Map($map, { zoom: 14, center: ${origin} });
-      
-      directionsRenderer.setMap(map);
-
-      const travelMode = "DRIVING";
-
-      function callback(response, status) {
-        (status == "OK")
-          ? directionsRenderer.setDirections(response)
-          : alert(status);
-      }
-
-      directionsService.route({ origin: ${origin}, destination: ${destination}, travelMode }, callback);
-    }
-  `;
-
-    this.$refs.mapContainer
-      .appendChild(googleMapScript)
-      .appendChild(initMapScript);
+    this.$refs.mapContainer.appendChild(iframe)
   }
 };
 </script>
