5fb66b857ad1c7ce7f546135dd5b74cea47beb08
Roiboon Chaiyachit <roiboon.cha@tourkrub.co>
2020-04-05 13:55:11 +0700

    Integrate bc api to bc page.

--- web/src/components/BC.vue
+++ web/src/components/BC.vue
@@ -4,10 +4,25 @@
       <h1 class="display-4">Finding B and C</h1>
     </section>
     <section class="border rounded p-5 mb-5">
-      <p v-if="isLoading">Loading ...</p>
-      <p v-else>
-        {{ message }}
-      </p>
+      <div v-if="isLoading">Loading ...</div>
+      <div v-else>
+        <p>
+          <strong>Let</strong>
+        </p>
+        <p>
+          A = {{ a }}
+        </p>
+        <p>
+          A + B = {{ aPlusB }}
+        </p>
+        <p>
+          A + C = {{ aPlusC }}
+        </p>
+        <p><strong>Then</strong></p>
+        <p>
+          B = {{ b }} and C = {{ c }}
+        </p>
+      </div>
     </section>
   </div>
 </template>
@@ -18,11 +33,21 @@ import { fetchDataService } from '../services'
 export default {
   async mounted() {
     try {
-      const result = await fetchDataService('api/bc', 'BC')
+      const { b, c } = await fetchDataService({
+        path: 'bc',
+        key: 'BC',
+        method: 'POST',
+        body: {
+          a: this.a,
+          a_plus_b: this.aPlusB,
+          a_plus_c: this.aPlusC,
+        }
+      })
 
-      this.message = result
+      this.b = b
+      this.c = c
     } catch (error) {
-      this.message = error
+      this.error = error
     } finally {
       this.isLoading = false
     }
@@ -30,7 +55,12 @@ export default {
   data() {
     return {
       isLoading: true,
-      message: ''
+      a: 21,
+      aPlusB: 23,
+      aPlusC: -21,
+      b: 0,
+      c: 0,
+      error: null
     }
   }
 };
