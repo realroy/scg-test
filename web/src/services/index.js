bad34f2c21f5266039920828545055aa48c8d8da
Roiboon Chaiyachit <roiboon.cha@tourkrub.co>
2020-04-05 13:46:36 +0700

    Add api endpoint.

--- web/src/services/index.js
+++ web/src/services/index.js
@@ -1,6 +1,15 @@
 const DEFAULT_EXPIRED_IN = 1000 * 60 * 60 * 24 * 7
 
-export const fetchDataService = async (endpoint,key, expireIn = DEFAULT_EXPIRED_IN) => {
+export const fetchDataService = async ({
+  path,
+  key,
+  expireIn = DEFAULT_EXPIRED_IN,
+  method = 'GET',
+  body,
+  headers = {
+    'Content-Type': 'application/json'
+  }
+} = {}) => {
   const cache = localStorage.getItem(key);
   
   let data = JSON.parse(cache)
@@ -9,7 +18,16 @@ export const fetchDataService = async (endpoint,key, expireIn = DEFAULT_EXPIRED_
     return data.result;
   }
 
-  const res = await fetch(endpoint);
+  if (typeof body !== 'string') {
+    body = JSON.stringify(body)
+  }
+
+  const API_ENDPOINT = process.env.NODE_ENV !== 'development'
+    ? 'api'
+    : 'http://localhost:5000'
+
+  const url = `${API_ENDPOINT}/${path}`
+  const res = await fetch(url, { method, body, headers });
   const result = res.json();
 
   const expiredAt = Date.now() + expireIn
